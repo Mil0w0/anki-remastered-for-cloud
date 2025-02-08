354282b23ebe0ab3b0552d72f92fff0d799e04cb
Clarence HIRSCH <chirsch1@myges.fr>
2025-02-08 18:17:34 +0100

    Refactor levelUpCategory tests

--- anki-remastered-api/test/cards/card.entity.spec.ts
+++ anki-remastered-api/test/cards/card.entity.spec.ts
@@ -3,63 +3,28 @@ import {Category} from "../../src/cards/domain/category.enum";
 
 describe('Card', () => {
 
-    describe('levelUpCategory', () => {
-        it('should return card with level two', async () => {
-            const card = new Card(
-                "1",
-                Category.FIRST,
-                "Who is that Pokemon ?",
-                "It's PIKACHU !",
-                "Gaming"
-            );
-            const expectedCard = new Card(
-                "1",
-                Category.SECOND,
-                "Who is that Pokemon ?",
-                "It's PIKACHU !",
-                "Gaming"
-            );
+    it('should return card with the next category level', () => {
+        const card = new Card(
+            "1",
+            Category.FIRST,
+            "Who is that Pokemon ?",
+            "It's PIKACHU !",
+            "Gaming"
+        );
+        const categories: Category[] = Object.values(Category);
+        expect(categories.length).toBe(7);
 
+        for (let i = 0; i < 6; i++) {
+            let expectedCard = new Card(
+                "1",
+                categories[i + 1],
+                "Who is that Pokemon ?",
+                "It's PIKACHU !",
+                "Gaming"
+            );
             expect(card.levelUpCategory()).toEqual(expectedCard);
-        });
 
-        it('should return card with level three', async () => {
-            const card = new Card(
-                "1",
-                Category.SECOND,
-                "Who is that Pokemon ?",
-                "It's PIKACHU !",
-                "Gaming"
-            );
-            const expectedCard = new Card(
-                "1",
-                Category.THIRD,
-                "Who is that Pokemon ?",
-                "It's PIKACHU !",
-                "Gaming"
-            );
-
-            expect(card.levelUpCategory()).toEqual(expectedCard);
-        });
-
-        it('should return card with level four', async () => {
-            const card = new Card(
-                "1",
-                Category.THIRD,
-                "Who is that Pokemon ?",
-                "It's PIKACHU !",
-                "Gaming"
-            );
-            const expectedCard = new Card(
-                "1",
-                Category.FOURTH,
-                "Who is that Pokemon ?",
-                "It's PIKACHU !",
-                "Gaming"
-            );
-
-            expect(card.levelUpCategory()).toEqual(expectedCard);
-        });
+        }
     });
 
     it('should throw error when category is already at the max level', () => {
