0a45944d4832e848fcb56c2674e34b86099bdaca
Clarence HIRSCH <chirsch1@myges.fr>
2025-02-09 15:05:43 +0100

    Making tests pass

--- anki-remastered-api/src/quizz/services/quizz.service.ts
+++ anki-remastered-api/src/quizz/services/quizz.service.ts
@@ -1,4 +1,5 @@
 import {CardService} from "../../cards/services/card.service";
+import {Category} from "../../cards/domain/category.enum";
 
 export class QuizzService {
 
@@ -9,8 +10,9 @@ export class QuizzService {
         return this.cardService.getAllCards()
             .then(cards =>
                 cards.filter(
-                    card => card.lastResponseDate === null ||
-                        card.lastResponseDate < new Date(new Date().toLocaleDateString())
+                    card => card.lastResponseDate === null
+                        || (card.lastResponseDate < new Date(new Date().toLocaleDateString()) && (card.category === Category.FIRST || card.category === Category.SECOND))
+                        || (card.lastResponseDate <new Date(new Date(new Date().setDate(new Date().getDate() - 3)).toLocaleDateString()) && card.category === Category.THIRD)
                 )
             );
     }
