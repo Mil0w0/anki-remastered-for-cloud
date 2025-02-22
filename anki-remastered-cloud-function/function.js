6efee339b5f4a4e410b0e1b5c95e9b2e9cf493fe
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-02-22 18:22:19 +0100

    fix cloud func

--- anki-remastered-cloud-function/function.js
+++ anki-remastered-cloud-function/function.js
@@ -1,6 +1,18 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('compareAnswers', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.status(200).send({"isValid": true });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    if ('OPTIONS' == req.method) {
+        res.status(200).send();
+    }
+    if(!req.body['userAnswer'] || !req.body['cardAnswer'] ){
+        res.status(400).send("Bad request");
+    }
+    const userAnswer = req.body['userAnswer'].toLowerCase();
+    const cardAnswer = req.body['cardAnswer'].toLowerCase();
+
+    res.status(200).send({"isValid": userAnswer === cardAnswer });
 });
